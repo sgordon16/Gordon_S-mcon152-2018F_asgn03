fbdda0b12a8f705a557334cc53963be0872476ce
Shlomo Gordon <sgordon16@student.touro.edu>
2018-11-08 01:28:11 -0500

    added unit tests

--- src/mcon152Asgn03/IntArrayOperations.java
+++ src/mcon152Asgn03/IntArrayOperations.java
@@ -1,5 +1,25 @@
 package mcon152Asgn03;
 
 public class IntArrayOperations {
+	
+	public static int Sum(int[] x) {
+		 
+		return 0;
+	}
+	
+	public static int Max(int[] x) {
+		
+		return 0;
+	}
+	
+	public static int Min(int[] x) {
+		
+		return 0;
+	}
+	
+	public static int Range(int[] x) {
+		
+		return 0;
+	}
 
 }
